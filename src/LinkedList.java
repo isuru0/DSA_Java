c5b3e4836db4a58627bdafda499f4a8f0cd60689
isuru0 <isurushehara@gmail.com>
2024-04-14 10:22:40 +0530

    Add linked list classes

---
+++ src/LinkedList.java
@@ -0,0 +1,110 @@
+public class LinkedList {
+
+    static class Node {
+        int data;
+        Node next;
+
+        Node head = null;
+        Node tail = null;
+
+        //INSERT OPERATION
+        void add(int data) {
+            Node newNode = new Node();
+            newNode.data = data;
+            if(head != null) {
+                tail.next = newNode;
+                tail = newNode;
+            } else {
+                head = tail = newNode;
+            }
+        }
+
+        //REMOVE OPERATION
+        void remove() {
+            if(head == null) {
+                System.out.println("Can't remove elements because list is empty!");
+            } else if (head != tail) {
+                Node temp_head = head;
+                while (head != null) {
+                    try {
+                        if (head.next == tail) {
+                            tail = head;
+                            head.next = null;
+                            break;
+                        }
+                    } catch (Exception e) {
+                        System.out.println("Warning: " + e);
+                    }
+                    head = head.next;
+                }
+                head = temp_head;
+            } else {
+                head = null;
+                System.out.println("List is empty");
+            }
+        }
+
+        //SEARCH OPERATION
+        void search(int data) {
+            Node temp_head = head;
+            boolean found = false;
+            while (head != null) {
+                if(head.data == data)
+                    found = true;
+                head = head.next;
+            }
+            if(found)
+                System.out.println("The value is here!");
+            else
+                System.out.println("The value is not here!");
+        }
+
+        //PRINT OPERATION
+        void print() {
+            Node temp_head = head;
+            while(head != null) {
+                System.out.print(head.data +" ");
+                if(head.next != null) {
+                    head = head.next;
+                } else {
+                    break;
+                }
+            }
+            head = temp_head;
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        Node list = new Node();
+        list.add(10);
+        list.add(20);
+        list.add(30);
+        list.print();
+
+        list.add(40);
+        list.add(50);
+        list.print();
+
+        list.remove();
+        list.remove();
+        list.remove();
+        list.remove();
+        list.remove();
+
+        list.remove();
+        list.remove();
+        list.print();
+
+        list.add(10);
+        list.add(20);
+        list.add(30);
+        list.add(40);
+        list.add(50);
+        list.print();
+
+        list.search(50);
+        list.search(1);
+    }
+
+}
---
+++ src/LinkedList_package.java
@@ -0,0 +1,26 @@
+//IMPORT LINKED LIST PACKAGE
+import java.util.LinkedList;
+
+public class LinkedList_package {
+    public static void main(String[] args) {
+
+        //CREATE A LINKED LIST
+        LinkedList<Integer> numbers = new LinkedList<>();
+
+        //INSERT DATA
+        numbers.add(10);
+        numbers.add(2, 20);
+        numbers.add(30);
+        numbers.add(40);
+        numbers.add(50);
+
+        //REMOVE DATA
+        numbers.remove();
+        numbers.remove(3);
+
+        //INSERT OPTIONS
+        numbers.addFirst(5);
+        numbers.addLast(35);
+
+    }
+}
